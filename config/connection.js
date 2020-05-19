91ff9af16b3e9bcb3569087af53aad3830374a50
Rachael Gladwin <rachaelgladwin@Rachaels-iMac.local>
2020-05-18 18:14:41 -0600

    working .env

---
+++ config/connection.js
@@ -0,0 +1,54 @@
+var mysql = require("mysql");
+require('dotenv').config();
+
+// Make connection.
+var connection;
+
+var db = {
+    development: {
+        username: process.env.user,
+        password: process.env.password,
+        database: process.env.database,
+        host: process.env.host,
+        dialect: 'mysql'
+    },
+    test: {
+        username: process.env.user,
+        password: process.env.password,
+        database: process.env.database,
+        host: process.env.host,
+        dialect: 'mysql'
+    },
+    production: {
+        username: process.env.user,
+        password: process.env.password,
+        database: process.env.database,
+        host: process.env.host,
+        dialect: 'mysql'
+    }
+}
+
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.DB_PASSWORD,
+        database: "burgers_db"
+    });
+}
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+
+// Export connection for our ORM to use.
+module.exports = connection;
